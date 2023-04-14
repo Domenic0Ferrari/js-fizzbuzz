61458d075ba2f2e7c100176f2033c30701c5d53e
Domenic0Ferrari <domenico99ferrari@gmail.com>
2023-04-14 15:56:24 +0200

    add "myStyle" in DOM

--- assets/script.js
+++ assets/script.js
@@ -17,19 +17,18 @@ let FizzBuzz = 'fizzbuzz'
 for (let i = 1; i <= 100; i++) {
     if (i % 15== 0){
        console.log(FizzBuzz);
-       eleGrid.innerHTML = eleGrid.innerHTML + `<div class="cell">${FizzBuzz}</div>`
+       eleGrid.innerHTML = eleGrid.innerHTML + `<div class="cell-15">${FizzBuzz}</div>`
     } 
     else if (i % 3 == 0){
         console.log(Fizz);
-        eleGrid.innerHTML = eleGrid.innerHTML + `<div class="cell">${Fizz}</div>`
+        eleGrid.innerHTML = eleGrid.innerHTML + `<div class="cell-3">${Fizz}</div>`
     } 
     else if (i % 5 == 0){
         console.log(Buzz);
-        eleGrid.innerHTML = eleGrid.innerHTML + `<div class="cell">${Buzz}</div>`
+        eleGrid.innerHTML = eleGrid.innerHTML + `<div class="cell-5">${Buzz}</div>`
     } 
     else{
        console.log(i);
        eleGrid.innerHTML = eleGrid.innerHTML + `<div class="cell">${i}</div>`;
     }
 }
-// rid.innerHTML = eleGrid.innerHTML + `<div class="cell">${i}</div>`;
