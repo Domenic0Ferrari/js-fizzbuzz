77afda41bdd463ae7b4f1b2ff162c77abf7b7b74
Domenic0Ferrari <domenico99ferrari@gmail.com>
2023-06-22 23:31:26 +0200

    fix style and ciclo in script.js

--- assets/script.js
+++ assets/script.js
@@ -10,25 +10,22 @@ Per sapere se un numero è divisibile per un altro usare '%'
 */
 
 const eleGrid = document.querySelector('.grid');
-let Fizz = 'fizz'
-let Buzz = 'buzz'
-let FizzBuzz = 'fizzbuzz'
 
 for (let i = 1; i <= 100; i++) {
-    if (i % 15== 0){
-       console.log(FizzBuzz);
-       eleGrid.innerHTML = eleGrid.innerHTML + `<div class="cell-15">${FizzBuzz}</div>`
-    } 
-    else if (i % 3 == 0){
-        console.log(Fizz);
-        eleGrid.innerHTML = eleGrid.innerHTML + `<div class="cell-3">${Fizz}</div>`
-    } 
-    else if (i % 5 == 0){
-        console.log(Buzz);
-        eleGrid.innerHTML = eleGrid.innerHTML + `<div class="cell-5">${Buzz}</div>`
-    } 
-    else{
-       console.log(i);
-       eleGrid.innerHTML = eleGrid.innerHTML + `<div class="cell">${i}</div>`;
+    if (i % 15 == 0) {
+        //    console.log(FizzBuzz);
+        eleGrid.innerHTML += `<div class="cell fizzbuzz">FizzBuzz</div>`
+    }
+    else if (i % 3 == 0) {
+        // console.log(Fizz);
+        eleGrid.innerHTML += `<div class="cell fizz">Fizz</div>`
+    }
+    else if (i % 5 == 0) {
+        // console.log(Buzz);
+        eleGrid.innerHTML += `<div class="cell buzz">Buzz</div>`
+    }
+    else {
+        //    console.log(i);
+        eleGrid.innerHTML += `<div class="cell">${i}</div>`;
     }
 }
